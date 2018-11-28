6134c485741fde2325cbaa00aa43a96bc043d6b5
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-11-28 12:25:35 -0600

    Updated Sing help and added output gen script

---
+++ runscripts/make_outputs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Generate a number of useful extra outputs from Freesurfer results: PDF page,
+# regional volumes, and Nifti images.
+
+
+# Find center of mass of left and right thalamus, left and right hippocampus, brainstem
+
+
+# Whole brain: cor, ax, sag through thalamus, hippocampus, brainstem
+
+# Zooms: cor, ax, sag through thalamus, hippocampus, brainstem
+
+
+# Make useful resources:
+
+# VOLUMES
+#    ThalamicNuclei.v10.T1.volumes.txt
+#    brainstemSsVolumes.v12.txt
+#    lh.amygNucVolumes-T1.v21.txt
+#    lh.hippoSfVolumes-T1.v21.txt
+#    rh.amygNucVolumes-T1.v21.txt
+#    rh.hippoSfVolumes-T1.v21.txt
+
+# NII_THALAMUS
+#    ThalamicNuclei.v10.T1.FSvoxelSpace.nii.gz
+
+# NII_BRAINSTEM
+#    brainstemSsLabels.v12.FSvoxelSpace.nii.gz
+
+# NII_HIPP_AMYG
+#    lh.hippoAmygLabels-T1.v21.FSvoxelSpace.nii.gz
+#    rh.hippoAmygLabels-T1.v21.FSvoxelSpace.nii.gz
+#    lh.hippoAmygLabels-T1.v21.HBT.FSvoxelSpace.nii.gz
+#    rh.hippoAmygLabels-T1.v21.HBT.FSvoxelSpace.nii.gz
+#    lh.hippoAmygLabels-T1.v21.FS60.FSvoxelSpace.nii.gz
+#    rh.hippoAmygLabels-T1.v21.FS60.FSvoxelSpace.nii.gz
+#    lh.hippoAmygLabels-T1.v21.CA.FSvoxelSpace.nii.gz
+#    rh.hippoAmygLabels-T1.v21.CA.FSvoxelSpace.nii.gz
+
