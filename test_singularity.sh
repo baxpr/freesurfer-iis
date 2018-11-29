55c6f5d9d622709cbd1279119396771ce4ec869d
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-11-29 16:01:02 -0600

    New sing test

--- test_singularity.sh
+++ test_singularity.sh
@@ -4,7 +4,7 @@ singularity run \
 --bind freesurfer_license.txt:/usr/local/freesurfer/license.txt \
 --bind INPUTS:/INPUTS \
 --bind OUTPUTS:/OUTPUTS \
-test.simg \
+baxpr-freesurfer-singularity-master-dev.simg \
 --outdir /OUTPUTS \
 --t1_nii /INPUTS/T1.nii.gz \
 --project TESTPROJ \
@@ -12,12 +12,3 @@ test.simg \
 --session TESTSESS \
 --scan TESTSCAN
 
-
-exit 0
-
-singularity shell \
---cleanenv \
---bind freesurfer_license.txt:/usr/local/freesurfer/license.txt \
---bind INPUTS:/INPUTS \
---bind OUTPUTS_tess_error:/OUTPUTS \
-test.simg
