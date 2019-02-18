c5e242a4476beda67ee246d5ac2d1cd97150fd22
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-02-18 15:18:26 -0600

    Page 1 text

--- src/page1.sh
+++ src/page1.sh
@@ -35,3 +35,17 @@ lh_lat_pial.png lh_med_pial.png rh_lat_pial.png rh_med_pial.png \
 lh_lat_white.png lh_med_white.png rh_lat_white.png rh_med_white.png \
 -tile 2x -quality 100 -background white -gravity center \
 -trim -border 20 -bordercolor white -resize 600x page1.png
+
+# Add info
+convert page1.png \
+-background white -resize 1194x1479 -extent 1194x1479 \
+-bordercolor white -border 15 \
+-gravity SouthEast -background white -splice 0x15 -pointsize 24 \
+-annotate +15+10 "$(date)" \
+-gravity SouthWest -annotate +15+10 \
+`cat $FREESURFER_HOME/build-stamp.txt` \
+-gravity NorthWest -background white -splice 0x60 -pointsize 24 -annotate +15+10 \
+'FreeSurfer recon-all' \
+-gravity NorthEast -pointsize 24 -annotate +15+10 \
+"${SUBJECT}" \
+page1.png
