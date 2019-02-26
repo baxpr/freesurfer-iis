2681a115b51b28e367bb8f4735a276a7a91bf261
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-02-26 17:39:11 -0600

    page 2

---
+++ src/page2.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+# Slices with surface - series of coronal, series of axial.
+#
+# Subcortical also.
+
+
+# FIXME These settings are for testing only
+export FREESURFER_HOME=/usr/local/freesurfer
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
+export SUBJECTS_DIR=`pwd`/../OUTPUTS
+export SUBJECT=SUBJECT
+
+# Script location
+#RSCR=/opt/runscripts
+RSCR=`pwd`
+
+# Directories
+TMP="${SUBJECTS_DIR}"/"${SUBJECT}"/tmp
+MRI="${SUBJECTS_DIR}"/"${SUBJECT}"/mri
+SURF="${SUBJECTS_DIR}"/"${SUBJECT}"/surf
+
+# Create the aseg without wm or cerebral gm
+mri_binarize --i "${MRI}"/aseg.mgz --o "${TMP}"/aseg.sub.mgz \
+--replace 2  0 --replace 3 0 --replace 4 0 \
+--replace 41 0 --replace 42 0 --replace 43 0
+
+cd "${TMP}"
+
+# Axial slices
+for S in 80 100 120 140 160 180 ; do
+    freeview \
+    -viewsize 800 800 --layout 1 --zoom 1 --viewport axial \
+    -v "${MRI}"/T1.mgz \
+    -v "${TMP}"/aseg.sub.mgz:visible=1:colormap=lut \
+    -f "${SURF}"/lh.white:edgecolor=turquoise:edgethickness=1 \
+    -f "${SURF}"/lh.pial:edgecolor=red:edgethickness=1 \
+    -f "${SURF}"/rh.white:edgecolor=turquoise:edgethickness=1 \
+    -f "${SURF}"/rh.pial:edgecolor=red:edgethickness=1 \
+    -slice  "$S" "$S" "$S" -ss axi_"$S".png
+done
+
+# Coronal slices
+for S in 50 75 100 125 150 175 ; do
+    freeview \
+    -viewsize 800 800 --layout 1 --zoom 1 --viewport coronal \
+    -v "${MRI}"/T1.mgz \
+    -v "${TMP}"/aseg.sub.mgz:visible=1:colormap=lut \
+    -f "${SURF}"/lh.white:edgecolor=turquoise:edgethickness=1 \
+    -f "${SURF}"/lh.pial:edgecolor=red:edgethickness=1 \
+    -f "${SURF}"/rh.white:edgecolor=turquoise:edgethickness=1 \
+    -f "${SURF}"/rh.pial:edgecolor=red:edgethickness=1 \
+    -slice  "$S" "$S" "$S" -ss cor_"$S".png
+done
+
+# Trim, change background to white, resize
+for p in axi_*png cor*png ; do
+	convert ${p} \
+	-fuzz 5% -fill none -draw "color 0,0 floodfill" -background white \
+	${p}
+done
+
+# Layout
+montage -mode concatenate \
+axi_80.png axi_100.png axi_120.png \
+axi_140.png axi_160.png axi_180.png \
+cor_50.png cor_75.png cor_100.png \
+cor_125.png cor_150.png cor_175.png \
+-tile 3x4 -quality 100 -background white -gravity center \
+-trim -border 10 -bordercolor white -resize 600x twelve.png
+
+
+
+# Add info
+# 8.5 x 11 at 144dpi is 1224 x 1584
+# inside 15px border is 1194 x 1554
+convert \
+-size 1224x1584 xc:white \
+-gravity center \( twelve.png -resize 1194x1454 \) -geometry +0+0 -composite \
+-gravity NorthEast -pointsize 24 -annotate +15+10 "recon-all" \
+-gravity SouthEast -pointsize 24 -annotate +15+10 "$(date)" \
+-gravity SouthWest -annotate +15+10 `cat $FREESURFER_HOME/build-stamp.txt` \
+-gravity NorthWest -pointsize 24 -annotate +15+10 "${SUBJECT}" \
+page3.png
+
+
