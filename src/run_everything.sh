fd6fc7211a3f0b3d2f1299e4166963b869ca67a7
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-09-10 13:30:45 -0500

    7.2

--- src/run_everything.sh
+++ src/run_everything.sh
@@ -2,83 +2,43 @@
 #
 # Full T1 pipeline:
 #    recon-all
-#    segmentThalamicNuclei.sh
-#    segmentHA_T1.sh
-#    segmentBS.sh
+#    volume computations/reformatting
+#    PDF creation
 
 # Defaults
-export T1_NII=/INPUTS/T1.nii.gz
-export PROJECT=UNK_PROJ
-export SUBJECT=UNK_SUBJ
-export SESSION=UNK_SESS
-export SCAN=UNK_SCAN
-export OUTDIR=/OUTPUTS
+export t1_niigz=/INPUTS/t1.nii.gz
+export label_info="UNKNOWN SCAN"
+export out_dir=/OUTPUTS
 
 # Parse inputs
 while [[ $# -gt 0 ]]
 do
   key="$1"
   case $key in
-    --t1_nii)
-        T1_NII="$2"
-        shift; shift
-        ;;
-    --project)
-        PROJECT="$2"
-        shift; shift
-        ;;
-    --subject)
-        SUBJECT="$2"
-        shift; shift
-        ;;
-    --session)
-        SESSION="$2"
-        shift; shift
-        ;;
-    --scan)
-        SCAN="$2"
-        shift; shift
-        ;;
-    --outdir)
-        OUTDIR="$2"
-        shift; shift
-        ;;
+    --t1_niigz)
+        export t1_niigz="$2"; shift; shift;;
+    --label_info)
+        export label_info="$2"; shift; shift;;
+    --out_dir)
+        export out_dir="$2"; shift; shift;;
     *)
-		echo Unknown argument $key
-        shift
-        ;;
+		echo "Unknown argument $key"; shift;;
   esac
 done
 
 # See what we got
-echo T1_NII      = "${T1_NII}"
-echo PROJECT     = "${PROJECT}"
-echo SUBJECT     = "${SUBJECT}"
-echo SESSION     = "${SESSION}"
-echo SCAN        = "${SCAN}"
-echo OUTDIR      = "${OUTDIR}"
+echo t1_niigz    = "${t1_niigz}"
+echo label_info  = "${label_info}"
+echo out_dir     = "${out_dir}"
 
 # Freesurfer setup
 source "${FREESURFER_HOME}"/SetUpFreeSurfer.sh
-export SUBJECTS_DIR="${OUTDIR}"
-export SUBJECT="${PROJECT}-x-${SUBJECT}-x-${SESSION}-x-${SCAN}"
-
-# Script location
-export SRC=/opt/src
+export SUBJECTS_DIR="${out_dir}"
 
 # recon-all
-recon-all -all -i "${T1_NII}" -s "${SUBJECT}"
-
-# Thalamus
-segmentThalamicNuclei.sh "${SUBJECT}" "${SUBJECTS_DIR}"
-
-# Hippocampus/amygdala
-segmentHA_T1.sh "${SUBJECT}" "${SUBJECTS_DIR}"
-
-# Brainstem
-segmentBS.sh "${SUBJECT}" "${SUBJECTS_DIR}"
+recon-all -all -i "${t1_niigz}" -s SUBJECT
 
 # Produce additional outputs and organize
-${SRC}/volume_computations.sh
-${SRC}/create_MM_labelmaps.sh
-${SRC}/make_outputs.sh
+volume_computations.sh
+create_MM_labelmaps.sh
+make_outputs.sh
