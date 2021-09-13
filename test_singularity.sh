cb1ce6a4bf10f6704c9a6c73b09f557da2db2562
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-09-13 16:34:23 -0500

    recon-all works

--- test_singularity.sh
+++ test_singularity.sh
@@ -1,52 +1,10 @@
 
-# Full pipeline
-singularity run \
---cleanenv \
---bind freesurfer_license.txt:/usr/local/freesurfer/license.txt \
---bind INPUTS:/INPUTS \
---bind OUTPUTS:/OUTPUTS \
-baxpr-freesurfer-singularity-master-dev2.simg \
---outdir /OUTPUTS \
---t1_nii /INPUTS/T1.nii.gz \
---project TESTPROJ \
---subject TESTSUBJ \
---session TESTSESS \
---scan TESTSCAN \
-> OUTPUTS/testrun.log 2>&1 
-
-
-
-exit 0
-
-
-#############################################################################
-# To test post-recon stuff only
-
-# Shell in
 singularity shell \
---cleanenv \
---bind freesurfer_license.txt:/usr/local/freesurfer/license.txt \
---bind INPUTS:/INPUTS \
---bind OUTPUTS:/OUTPUTS \
---bind `pwd`:/opt/wkdir \
-baxpr-freesurfer-singularity-master-dev2.simg
+    --contain \
+    --cleanenv \
+    --bind freesurfer_license.txt:/usr/local/freesurfer/license.txt \
+    --bind INPUTS:/INPUTS \
+    --bind OUTPUTS:/OUTPUTS \
+    --bind OUTPUTS:/tmp \
+    test.simg
 
-# Set up
-source "${FREESURFER_HOME}"/SetUpFreeSurfer.sh
-export SUBJECTS_DIR=/OUTPUTS
-export SUBJECT=SUBJECT
-
-# Location of edited scripts
-export SRC=/opt/wkdir/src
-
-# All post-recon stuff
-${SRC}/volume_computations.sh
-${SRC}/create_MM_labelmaps.sh
-xvfb-run --server-num=$(($$ + 99)) \
-    --server-args='-screen 0 1600x1200x24 -ac +extension GLX' \
-    ${SRC}/make_outputs.sh
-
-# Just the last bit of make_outputs
-xvfb-run --server-num=$(($$ + 99)) \
-    --server-args='-screen 0 1600x1200x24 -ac +extension GLX' \
-    ${SRC}/make_slice_screenshots.sh
